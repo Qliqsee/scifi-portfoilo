e8144e894a09c9469ae14108501dbab7816ea1b8
Iyanu <agboolasolomoniyanu@gmail.com>
2024-11-10 23:14:37 +0100

    image path issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ import { withStyles } from "@/hoc/withStyles";
 import { Fader } from "@/component/Fader";
 import styled from "@emotion/styled";
 import Image from "next/image";
-import imageUrl from "@/images/qliqsee.jpg";
+import imageUrl from "../../images/qliqsee.jpg";
 
 const styles = (theme: any) => ({
   root: {},
